0d15ed57265b396b55ec8f07b1b9f2c0f648ee07
alhilalfathi <132118871+alhilalfathi@users.noreply.github.com>
2026-01-02 10:55:24 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const fs = require(`fs`)
 const bands = ["Niki", "Hindia", "Noah", "OneDirection", "JustinBieber"]
 let artist 
 
-fs.mkdir(`Music`,function(){
-        
+fs.mkdir(`Music`,function(err){
+        if(err){
+            console.log(`Gagal membuat file "Music"` )
+        }else console.log(`Berhasil membuat file "Music"`)
     })
 for(i=0; i<bands.length; i++){
     artist = bands[i]
     // console.log(artist)
-    fs.mkdir(`Music/${artist}`,function(error){
-        if(error){
+    fs.mkdir(`Music/${artist}`,function(err){
+        if(err){
             console.log("Gagal membuat file")
         }else console.log("Berhasil membuat file")
 
